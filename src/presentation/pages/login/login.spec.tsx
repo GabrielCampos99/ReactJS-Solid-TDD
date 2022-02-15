0eb70509d3bacc7cace5dbf4b5db58cfc2239c10
Gabriel Campos <gabriel.campos@alicerceedu.com.br>
2022-02-14 21:25:04 -0300

    test: ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,9 +3,12 @@ import { render, screen } from '@testing-library/react'
 import Login from './login'
 
 describe('Login Component', () => {
-  test('Should not render spinner and error on start', () => {
+  test('Should start with inital state', () => {
     render(<Login />)
 
     expect(screen.getByTestId('error-wrap').childElementCount).toBe(0)
+    expect(screen.getByRole('button')).toHaveProperty('disabled')
+    expect(screen.getByPlaceholderText(/Digite seu e-mail/i))
+    expect(screen.getByPlaceholderText(/Digite sua senha/i))
   })
 })
